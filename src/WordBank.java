635ce5bbdb16c57bbad9ccf01b4b70be3371774e
aljalousd <aljalousd@msoe.edu>
2024-02-14 17:55:51 -0600

    Class updated to read target/valid word files

--- src/WordBank.java
+++ src/WordBank.java
@@ -1,25 +1,53 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class WordBank {
-    private List<Word> targetWords = new ArrayList<>();
-    private List<Word> validWords = new ArrayList<>();
+    private final List<Word> targetWords = new ArrayList<>();
+    private final List<Word> validWords = new ArrayList<>();
 
     public WordBank() {
-
+        updateTargetBank(new File("./wordle-official.txt"));
+        updateValidBank(new File("./wordle-full.txt"));
     }
 
     public WordBank(File targetFile, File validFile) {
-        //TODO: Implement me!
+        updateTargetBank(targetFile);
+        updateValidBank(validFile);
     }
 
-    public void updateTargetBank(String targetFile) {
-        //TODO: Implement me!
+    public void updateTargetBank(File targetFile) {
+        try (Scanner in = new Scanner(targetFile)) {
+            while (in.hasNext()) {
+                String wordStr = in.nextLine();
+                Word wordObj = new Word(wordStr.length());
+
+                for (int i = 0; i < wordStr.length(); ++i) {
+                    wordObj.pushChar(wordStr.charAt(i));
+                }
+                targetWords.add(wordObj);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File cannot be found or does not exist.");
+        }
     }
 
-    public void updateValidBank(String validFile) {
-        //TODO: Implement me!
+    public void updateValidBank(File validFile) {
+        try (Scanner in = new Scanner(validFile)) {
+            while (in.hasNext()) {
+                String wordStr = in.nextLine();
+                Word wordObj = new Word(wordStr.length());
+
+                for (int i = 0; i < wordStr.length(); ++i) {
+                    wordObj.pushChar(wordStr.charAt(i));
+                }
+                validWords.add(wordObj);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File cannot be found or does not exist.");
+        }
     }
 
     public CharValidity[] getCorrect(Word word) {
