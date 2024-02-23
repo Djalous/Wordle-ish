a1fa2642cf924743be227f4495b71e3f6c029534
Theresa Kettner <kettnert@msoe.edu>
2024-02-23 16:19:26 -0600

    Added test to ensure only 5 letter guesses are stored

---
+++ tests/LetterGuessTests.java
@@ -0,0 +1,21 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LetterGuessTests {
+
+    @Test
+    public void testPushChar_ValidLength(){
+        Word word = new Word(5);
+        for (char c = 'a'; c <= 'e'; c++ ) {
+            word.pushChar(c);
+        }
+
+        assertEquals("abcde", word.toString());
+    }
+
+
+}
