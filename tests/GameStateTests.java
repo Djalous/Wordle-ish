4763ee0461562a4f3c4a9b50b6453f70c8479a90
LazarJ42 <70660001+LazarJ42@users.noreply.github.com>
2024-03-07 09:48:05 -0600

    started gamestate tests

---
+++ tests/GameStateTests.java
@@ -0,0 +1,29 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Optional;
+
+
+public class GameStateTests {
+    static GameState gameState;
+    @BeforeClass
+    public static void setUp() {
+        gameState = new GameState(new Word("Apple"));
+    }
+    @Test
+    public void updateCurrentGuessTest() {
+        gameState.updateCurrentGuess(new Word("Hello"));
+        Assert.assertEquals(new Word("Hello"), gameState.getCurrentGuess());
+        Assert.assertEquals(1, gameState.getGuessCount());
+        gameState.updateCurrentGuess(new Word("Super"));
+        Assert.assertEquals(new Word("Super"), gameState.getCurrentGuess());
+        Assert.assertEquals(2, gameState.getGuessCount());
+        gameState.updateCurrentGuess(new Word("Hello"));
+        Assert.assertTrue(gameState.getGuessList().containsKey((new Word("Hello")).toString()));
+        Assert.assertEquals(2, gameState.getGuessList().get(new Word("Hello").toString())); // fix
+        //Assert.assertFalse(gameState.getGuessList().containsKey((new Word("Super")).toString()));
+    }
+
+}
