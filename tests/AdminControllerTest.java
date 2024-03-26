106abe65cd58a24e7847f909faa111dd4bf0d5f5
Theresa Kettner <kettnert@msoe.edu>
2024-03-25 20:37:58 -0500

    Test update word bank after successful upload of new files

--- tests/AdminControllerTest.java
+++ tests/AdminControllerTest.java
@@ -1,12 +1,38 @@
-
+import main.Word;
+import main.WordBank;
 import org.junit.jupiter.api.Test;
-import java.nio.file.InvalidPathException;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AdminControllerTest {
+    static WordBank wordBank;
 
+    /**
+     * This test checks if the updateWordBank method works correctly after upload of new files.
+     * @throws FileNotFoundException if the file does not exist
+     */
     @Test
-    public void testUpdateBank() {
+    public void testUpdateWordBank() throws FileNotFoundException {
+        // Create an instance of Controller
+        //Controller controller = new Controller();
+
+        wordBank = new WordBank();
+        Word word = new Word("satin");
+
+        // Create valid File instances
+        File validTargetBankFile = new File("./wordle-full.txt");
+        File validVocabBankFile = new File("./wordle-official.txt");
+
+        wordBank = new WordBank(validTargetBankFile, validVocabBankFile);
+        assertTrue(wordBank.isValid(word));
+
+        // we expect false here because abcde is not a valid word
+        Word invalidWord = new Word("abcde");
+        assertFalse(wordBank.isValid(invalidWord));
+        
 
     }
 }
